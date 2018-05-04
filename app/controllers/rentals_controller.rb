b0a57deb4b1a0add70389ce3b62b6ead56b84b17
Bence Széplaki <szeplaki.bence@outlook.com>
2018-05-04 22:59:46 +0200

    Added delete funtionality to RentalsController, code refractoring.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -19,15 +19,16 @@ class RentalsController < ApplicationController
 
   def create
     response = send_create_rental(rental_params)
-    response_status = response.status
-    response_body = JSON.parse(response.body)
-    if response_status == 201
-      flash[:success] = 'Rental added successfully'
-      redirect_to new_rental_path
-    else
-      flash[:danger] = response_body['message']
-      redirect_to new_rental_path
-    end
+    if_successful_redirect(
+      {
+        status: 201,
+        response_status: response.status,
+        err_messages: JSON.parse(response.body),
+        success_text: 'Rental successfully created!',
+        success_path: new_rental_path,
+        failure_path: new_rental_path
+      }
+    )
   end
 
   def edit
@@ -37,6 +38,17 @@ class RentalsController < ApplicationController
   end
 
   def destroy
+    response = send_destroy_rental(params) # Ezt meg meg kéne nezni
+    if_successful_redirect(
+      {
+        status: 204,
+        response_status: response.status,
+        err_messages: { 'message' => 'Error: ' + response.status.to_s },
+        success_text: 'Rental removed successfully!',
+        success_path: rentals_path,
+        failure_path: rentals_path
+      }
+    )
   end
 
 
@@ -59,6 +71,15 @@ private
                                       }
   end
 
+  def send_destroy_rental(params)
+    conn = Faraday.new(url: 'http://localhost:3000') do |c|
+      c.use Faraday::Request::UrlEncoded
+      c.use Faraday::Response::Logger
+      c.use Faraday::Adapter::NetHttp
+    end
+    conn.delete "/rentals/#{params[:id].to_s}"
+  end
+
   def get_rental_index()
     conn = Faraday.new(url: 'http://localhost:3000') do |c|
       c.use Faraday::Request::UrlEncoded
@@ -68,5 +89,15 @@ private
 
     response = conn.get '/rentals'
     JSON.parse(response.body)
+  end
+
+  def if_successful_redirect(options)
+    if options[:response_status] == options[:status]
+      flash[:success] = options[:success_text]
+      redirect_to options[:success_path]
+    else
+      flash[:danger] = options[:err_messages]['message']
+      redirect_to options[:failure_path]
+    end
   end
 end
