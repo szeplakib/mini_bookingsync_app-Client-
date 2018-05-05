d848f7363af54396332d6255fd27331bf1194b7e
Bence Széplaki <szeplaki.bence@outlook.com>
2018-05-05 15:02:44 +0200

    Requests have been refractored in the client for RentalsController, Edit action, and Rental model have been refractored on client side too.

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,11 +3,11 @@ class RentalsController < ApplicationController
   include RentalsHelper
 
   def book_a_rental
-    @rentals = get_rental_index
+    @rentals = send_rental_index_request
   end
 
   def index
-    @rentals = get_rental_index
+    @rentals = send_rental_index_request
   end
 
   def show
@@ -18,7 +18,7 @@ class RentalsController < ApplicationController
   end
 
   def create
-    response = send_create_rental(rental_params)
+    response = send_create_rental_request(rental_params)
     if_successful_redirect(
       {
         status: 201,
@@ -32,14 +32,15 @@ class RentalsController < ApplicationController
   end
 
   def edit
-    response = send_show_rental(params)
-    @rental = Rental.new
-    @rental.name = JSON.parse(response.body)['name']
-    @rental.daily_rate = JSON.parse(response.body)['daily_rate'] # ez a resz okadek
+    response = send_show_rental_request(params)
+    @rental = Rental.new(
+      name: JSON.parse(response.body)['name'],
+      daily_rate: JSON.parse(response.body)['daily_rate']
+    )
   end
 
   def update
-    response = send_update_rental(params) # Ezt meg meg kéne nezni
+    response = send_update_rental_request(params) # Ezt meg meg kéne nezni
     if_successful_redirect(
       {
         status: 204,
@@ -53,7 +54,7 @@ class RentalsController < ApplicationController
   end
 
   def destroy
-    response = send_destroy_rental(params) # Ezt meg meg kéne nezni
+    response = send_destroy_rental_request(params) # Ezt meg meg kéne nezni
     if_successful_redirect(
       {
         status: 204,
@@ -66,65 +67,46 @@ class RentalsController < ApplicationController
     )
   end
 
+  private
 
-private
+  def connect_api
+    Faraday.new(url: 'http://localhost:3000') do |c|
+      c.use Faraday::Request::UrlEncoded
+      c.use Faraday::Response::Logger
+      c.use Faraday::Adapter::NetHttp
+    end
+  end
 
   def rental_params
     params.require(:rental).permit(:name, :daily_rate)
   end
 
-  def send_create_rental(rental_params)
-    conn = Faraday.new(url: 'http://localhost:3000') do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use Faraday::Response::Logger
-      c.use Faraday::Adapter::NetHttp
-    end
-    conn.post '/rentals',
+  def send_create_rental_request(rental_params)
+    connect_api.post '/rentals',
                                      { 
                                       name: rental_params[:name],
                                       daily_rate: rental_params[:daily_rate]
                                       }
   end
 
-  def send_show_rental(params)
-    conn = Faraday.new(url: 'http://localhost:3000') do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use Faraday::Response::Logger
-      c.use Faraday::Adapter::NetHttp
-    end
-    conn.get "/rentals/#{params[:id]}"
+  def send_show_rental_request(params)
+    connect_api.get "/rentals/#{params[:id]}"
   end
 
-  def send_destroy_rental(params)
-    conn = Faraday.new(url: 'http://localhost:3000') do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use Faraday::Response::Logger
-      c.use Faraday::Adapter::NetHttp
-    end
-    conn.delete "/rentals/#{params[:id].to_s}"
+  def send_destroy_rental_request(params)
+    connect_api.delete "/rentals/#{params[:id].to_s}"
   end
 
-  def send_update_rental(params)
-    conn = Faraday.new(url: 'http://localhost:3000') do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use Faraday::Response::Logger
-      c.use Faraday::Adapter::NetHttp
-    end
-    conn.patch "/rentals/#{params[:id].to_s}",
+  def send_update_rental_request(params)
+    connect_api.patch "/rentals/#{params[:id].to_s}",
                                       { 
                                       name: rental_params[:name],
                                       daily_rate: rental_params[:daily_rate]
                                       }
   end
 
-  def get_rental_index()
-    conn = Faraday.new(url: 'http://localhost:3000') do |c|
-      c.use Faraday::Request::UrlEncoded
-      c.use Faraday::Response::Logger
-      c.use Faraday::Adapter::NetHttp
-    end
-
-    response = conn.get '/rentals'
+  def send_rental_index_request()
+    response = connect_api.get '/rentals'
     JSON.parse(response.body)
   end
 
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,7 +1,10 @@
 class Rental
-  include ActiveModel::Validations
   include ActiveModel::Conversion
   extend  ActiveModel::Naming
   attr_accessor :name,
                 :daily_rate
+  def initialize(arg = {name: nil, daily_rate: nil})
+    @name = arg[:name]
+    @daily_rate = arg[:daily_rate]
+  end
 end
